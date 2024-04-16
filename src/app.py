ce4be8bc4ca97478e07d29c995e5149980b62919
Marine B <missmarineb@orange.fr>
2024-04-15 22:52:15 -0500

    Added folder used to deliver built react using Flask, inspi from Loic's branch

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
 from back import *
 import json
@@ -6,12 +6,12 @@ import ast
 import io
 import atexit
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='react_build')
 cors = CORS(app)
 
 @app.route('/api/save', methods=['POST'])
 def save_history():
-    try: 
+    try:
         History = request.get_json().get('history')
         if (History != [[]]):
             print("\nSaving history to file...")
@@ -32,13 +32,13 @@ def receive_question():
         history = data.get('history')
         gpt4 = data.get('gpt4')
         print(gpt4)
-        if len(question)==0:
+        if len(question) == 0:
             question.append(" ") 
         print("question:", question)
         
         # Generation of the answer
-        (answer, sources) =  generate_answer(question, history, gpt4, deployment=deployment_name)
-        #print(sources)
+        (answer, sources) = generate_answer(question, history, gpt4, deployment=deployment_name)
+        # print(sources)
         sources_to_print = {}
         for src in sources:
             print(src)
@@ -61,6 +61,15 @@ def receive_question():
         return jsonify({'message': response})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     #load_history()
